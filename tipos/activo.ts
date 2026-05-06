6588d1b19d355b0cf34a1f7118bf5a7667a7c357
Sergio Escalona B <sergioluis2610@hotmail.com>
2026-05-06 14:04:26 -0500

    Diseño activo

---
+++ tipos/activo.ts
@@ -0,0 +1,12 @@
+
+export type Activo = {
+    id: number;
+  nombre: string;
+  categoria: string;
+  marca: string;
+  estado: string;
+  ubicacion: string | null;
+  modelo: string;
+  numero_serie: string;
+
+};
